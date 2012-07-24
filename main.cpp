20485addd9856c41391115e14e328299743c20eb
kn65op <kn65op@gmail.com>
2012-07-24 12:43:16 +0200

    Created project in netbeans

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: tomko
+ *
+ * Created on 24 lipiec 2012, 12:41
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+
+  return 0;
+}
+
