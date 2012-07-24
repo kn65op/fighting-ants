ab92ef3ef59291af5113631f4e5d6501482697b1
kn65op <kn65op@gmail.com>
2012-07-24 18:56:08 +0200

    Added Field

---
+++ include/Field.h
@@ -0,0 +1,31 @@
+/* 
+ * File:   Field.h
+ * Author: tomko
+ *
+ * Created on 24 lipiec 2012, 18:55
+ */
+
+#ifndef FIELD_H
+#define	FIELD_H
+
+/**
+ * Class reprezents one field from ground.
+ */
+class Field
+{
+public:
+  /**
+   * Default constructor.
+   */
+  Field();
+  Field(const Field& orig);
+  /**
+   * Default destructor.
+   */
+  virtual ~Field();
+private:
+
+};
+
+#endif	/* FIELD_H */
+
---
+++ src/Field.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   Field.cpp
+ * Author: tomko
+ * 
+ * Created on 24 lipiec 2012, 18:55
+ */
+
+#include "../include/Field.h"
+
+Field::Field()
+{
+}
+
+Field::Field(const Field& orig)
+{
+}
+
+Field::~Field()
+{
+}
+
