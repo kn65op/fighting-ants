12eb16d0fd1b2f30742d4bf78082ebea4824a046
kn65op <kn65op@gmail.com>
2012-07-24 15:48:01 +0200

    Removed example painting

--- src/Field.cpp
+++ src/Field.cpp
@@ -21,23 +21,6 @@ bool Field::on_draw(const Cairo::RefPtr<Cairo::Context>& cr)
   const int width = allocation.get_width();
   const int height = allocation.get_height();
 
-  // coordinates for the center of the window
-  int xc, yc;
-  xc = width / 2;
-  yc = height / 2;
-
-  cr->set_line_width(10.0);
-
-  // draw red lines out from the center of the window
-  cr->set_source_rgb(0.8, 0.0, 0.0);
-  cr->move_to(0, 0);
-  cr->line_to(xc, yc);
-  cr->line_to(0, height);
-  cr->move_to(xc, yc);
-  cr->line_to(width, yc);
-  cr->stroke();
-
   return true;
-
 }
 
