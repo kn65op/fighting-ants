63f20088967a511e6462ae60215eeccffba46eca
kn65op <kn65op@gmail.com>
2012-07-24 18:22:47 +0200

    AddedApplication controller

---
+++ include/ApplicationController.h
@@ -0,0 +1,30 @@
+/* 
+ * File:   ApplicationController.h
+ * Author: tomko
+ *
+ * Created on 24 lipiec 2012, 18:22
+ */
+
+#ifndef APPLICATIONCONTROLLER_H
+#define	APPLICATIONCONTROLLER_H
+
+/**
+ * Class
+ */
+class ApplicationController
+{
+public:
+  /**
+   * Default constructor.
+   */
+  ApplicationController();
+  /**
+   * Default destructor.
+   */
+  virtual ~ApplicationController();
+private:
+
+};
+
+#endif	/* APPLICATIONCONTROLLER_H */
+
---
+++ src/ApplicationController.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   ApplicationController.cpp
+ * Author: tomko
+ * 
+ * Created on 24 lipiec 2012, 18:22
+ */
+
+#include "../include/ApplicationController.h"
+
+ApplicationController::ApplicationController()
+{
+}
+
+ApplicationController::~ApplicationController()
+{
+}
+
