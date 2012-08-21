6e5598291fee4e98eedfd9619b60f32f7e79ffe3
kn65op <kn65op@gmail.com>
2012-08-21 19:08:32 +0200

    Add word overide

--- include/GroundArea.h
+++ include/GroundArea.h
@@ -32,7 +32,7 @@ protected:
    * @param cr Cario::Context where painting it's made.
    * @return true.
    */
-  virtual bool on_draw(const Cairo::RefPtr<Cairo::Context>& cr);
+  bool on_draw(const Cairo::RefPtr<Cairo::Context>& cr) override;
 };
 
 #endif	/* GROUND_AREA_H */
